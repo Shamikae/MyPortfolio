6a493d11829b4d33ece1ec9a88ee0c218a687e24
shamikae <e.shamika@yahoo.com>
2021-10-26 12:03:11 -0400

    Hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Hero from "../components/Hero";
 
 export default function Home() {
     return (
         <div>
+            <Hero />
             <h1>This is .......Home</h1>
         </div>
     )
