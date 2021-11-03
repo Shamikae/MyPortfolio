1f436850bf141d285b3fa0310234aa0ce70d85a1
shamikae <e.shamika@yahoo.com>
2021-11-03 05:39:01 -0400

    Feat: Services and Projects

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from "../components/Hero";
 import AboutSec from '../components/AboutSec';
+import ServiceSec from '../components/ServiceSec';
+import ProjectsSec from '../components/ProjectsSec';
 
 export default function Home() {
     return (
         <div>
             <Hero />
             <AboutSec />
+            <ServiceSec /> 
+            <ProjectsSec />
         </div>
     )
 }
