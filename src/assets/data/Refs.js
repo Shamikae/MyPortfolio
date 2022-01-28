3c4bf4245809582747e213bf0dffcfefe3404491
shamikae <e.shamika@yahoo.com>
2022-01-28 04:22:11 -0500

    fix

--- src/assets/data/Refs.js
+++ src/assets/data/Refs.js
@@ -4,24 +4,21 @@ const Refs = [
     name: 'Sha',
     title: 'CEO',
     org: 'Syber Web Design',
-    desc:
-      'Mika is a person of commitment. She is really good at what she does. I really enjoy her work. Especially her unique point of view of designing.',
+    desc: 'Mika is a person of commitment. She is really good at what she does. I really enjoy her work. Especially her unique point of view of designing.',
   },
   {
     id: 2,
     name: 'Kim',
     title: 'Co-founder',
     org: 'Kulture',
-    desc:
-      'Really amazing communication skills. Understood what I was trying to achieve. Really amazing high-quality work.',
+    desc: 'Really amazing communication skills. Understood what I was trying to achieve. Really amazing high-quality work.',
   },
   {
     id: 3,
     name: 'Sam',
     title: 'CEO',
     org: 'NineDesign',
-    desc:
-      'Really hard-working person. Delivered the work as promised and fast. My go to for future projects.',
+    desc: 'Really hard-working person. Delivered the work as promised and fast. My go to for future projects.',
   },
 ];
 export default Refs;
